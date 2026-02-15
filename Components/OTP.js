d40619a64f77dea6d912c044ee9736488b7e85b2
Phat <patlaksanai2548@gmail.com>
2026-02-15 23:46:04 +0700

    test api

--- Components/OTP.js
+++ Components/OTP.js
@@ -22,7 +22,7 @@ const OTP = ({ navigation, route }) => {
 
   useEffect(() => {
     if (autoResend) {
-      fetch("http://10.0.2.2:3000/api/resend-otp", {
+      fetch("https://bookapp-h41h.onrender.com/api/resend-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
@@ -63,7 +63,7 @@ const OTP = ({ navigation, route }) => {
 
     try {
       // อย่าลืมใส่ credentials: 'include' เพื่อให้ Server สร้าง Session Cookie ได้
-      const res = await fetch("http://10.0.2.2:3000/api/verify-otp", {
+      const res = await fetch("https://bookapp-h41h.onrender.com/api/verify-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, otp: otpCode }),
@@ -97,7 +97,7 @@ const OTP = ({ navigation, route }) => {
   // ✅ RESEND OTP
   const handleResend = async () => {
     try {
-      await fetch("http://10.0.2.2:3000/api/resend-otp", {
+      await fetch("https://bookapp-h41h.onrender.com/api/resend-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
--- Components/Profile.js
+++ Components/Profile.js
@@ -58,7 +58,7 @@ const Profile = ({ navigation }) => {
       const token = await AsyncStorage.getItem("token");
       console.log("TOKEN:", token);
       const res = await fetch(
-        'http://10.0.2.2:3000/api/upload-profile-pic',
+        'https://bookapp-h41h.onrender.com/api/upload-profile-pic',
         {
           method: 'POST',
           headers: {
@@ -94,7 +94,7 @@ const Profile = ({ navigation }) => {
   const handleLogout = async () => {
     try {
       // Note: Added timeout or error handling for network issues
-      const res = await fetch(`http://10.0.2.2:3000/api/logout`, { method: 'POST', credentials: 'include' });
+      const res = await fetch(`https://bookapp-h41h.onrender.com/api/logout`, { method: 'POST', credentials: 'include' });
       if (res.ok) {
         navigation.reset({ index: 0, routes: [{ name: 'Welcome' }] });
       } else {
@@ -106,7 +106,7 @@ const Profile = ({ navigation }) => {
   };
 
   useEffect(() => {
-    fetch(`http://10.0.2.2:3000/api/profile`, { credentials: 'include' })
+    fetch(`https://bookapp-h41h.onrender.com/api/profile`, { credentials: 'include' })
       .then(async res => {
         if (!res.ok) throw new Error('Unauthorized');
         const data = await res.json();
